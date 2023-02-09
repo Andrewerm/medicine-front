7f6ec29a11ffd5472aa5b56fa743a4f247572c55
Andrew <andrewerm@yandex.ru>
2023-02-09 18:21:12 +0300

    очистка ответов

--- src/components/SurveyReport.tsx
+++ src/components/SurveyReport.tsx
@@ -1,7 +1,8 @@
 import {Button, Card, Space, Typography} from "antd";
-import {useAppSelector} from "../hooks/reduxHooks";
+import {useAppDispatch, useAppSelector} from "../hooks/reduxHooks";
 import React, {useContext, useEffect} from "react";
 import {TopPanelContext} from "../hooks/topPanel";
+import {resetAnswers} from "../app/surveysSlice";
 
 interface ISurveyReportProps {
     onExit:  React.MouseEventHandler,
@@ -11,9 +12,11 @@ interface ISurveyReportProps {
 export const SurveyReport:React.FC<ISurveyReportProps> = ({surveyId, onExit}) => {
     const survey = useAppSelector(state => state.surveys.surveys.find(item => item.id === surveyId))
     const topPanelContext = useContext(TopPanelContext);
+    const dispatcher=useAppDispatch()
     useEffect(() => {
         const buttons = [<Button onClick={onExit}>ВЕРНУТЬСЯ К СПИСКУ ВОПРОСОВ</Button>]
         if (topPanelContext) topPanelContext.setButtons(buttons)
+        dispatcher(resetAnswers(surveyId))
     }, []);
     return (
         <>
